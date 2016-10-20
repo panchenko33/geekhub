3d9ff1d1de2f30672d0fe3b0ecc1f649c31c5436
panch <cas.neuer@gmail.com>
2016-10-21 00:26:18 +0300

    second commit

--- src/homework/example.java
+++ src/homework/example.java
@@ -1,7 +1,12 @@
 package homework;
 
-/**
- * Created by panchenko_33 on 21.10.2016.
- */
+import java.util.Scanner;
+
 public class example {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter telephone number +38093");
+        int i = sc.nextInt();
+        System.out.println("your number is: +380" + i);
+    }
 }

1392c093c48b6bed704b926fea791fbf58ebb0d1
panch <cas.neuer@gmail.com>
2016-10-26 21:08:50 +0300

    commit9

--- src/homework/homework1.java
+++ src/homework/homework1.java
@@ -5,54 +5,59 @@ import java.util.Scanner;
 public class homework1 {
     public static void main(String[] args) {
 
-        int num;
-        int b = 0;
-        int c = 0;
-        int a = 0;
-
         Scanner scn = new Scanner(System.in);
+
         System.out.print("enter telephone number +380");
 
         if (scn.hasNextInt()) {
-            num = scn.nextInt();
+
+            int num = scn.nextInt();
             int size = 0;
+            int i = 0;
+
             while (num != 0) {
-                b = b + num % 10;
+                i = i + num % 10;
                 num = num / 10;
                 size++;
             }
+
             if (size == 9) {
 
-
                 String result = "sum";
-                if (b == 1) {
+                if (i == 1) {
                     result = "one";
-                } else if (b == 2) {
+                } else if (i == 2) {
                     result = "two";
-                } else if (b == 3) {
+                } else if (i == 3) {
                     result = "three";
-                } else if (b == 4) {
+                } else if (i == 4) {
                     result = "four";
-                } else if (10 > b && b > 4) {
-                    result = " " + b;
-                } else if (b >= 10) {
-                    System.out.println("sum1 = " + b);
-                    while (b != 0) {
-                        a = a + b % 10;
-                        b = b / 10;
+                } else if (10 > i && i > 4) {
+                    result = " " + i;
+                } else if (i >= 10) {
+                    System.out.println("sum1 = " + i);
+                    while (i != 0) {
+                        num = num + i % 10;
+                        i = i / 10;
                     }
-                    result = " " + a;
+                    result = " " + num;
                 }
-                if (a >= 10) {
-                    System.out.println("sum2 = " + a);
-                    while (a != 0) {
-                        c = c + a % 10;
-                        a = a / 10;
+                if (num >= 10) {
+
+                    System.out.println("sum2 = " + num);
+
+                    while (num != 0) {
+                        i = i + num % 10;
+                        num = num / 10;
                     }
-                    result = " " + c;
+
+                    result = " " + i;
                 }
+
                 System.out.println("sum = " + result);
+
             } else System.out.println("you enter an incorrect telephone number");
+
         } else System.out.println ("it`s not a telephone number");
     }
 }

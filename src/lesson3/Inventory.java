d523e0aa338d6a2d63d908dd7ead4dd33e9ba3f4
panch <cas.neuer@gmail.com>
2016-11-06 01:12:05 +0200

    not finished

--- src/lesson3/Inventory.java
+++ src/lesson3/Inventory.java
@@ -3,6 +3,7 @@ package lesson3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by panchenko_33 on 05.11.2016.
@@ -29,13 +30,61 @@ public class Inventory {
         product.add(new Water("Fanta ", 16, 34));
         product.add(new Water("Sprite ", 16, 34));
 
-        for (int i = 0; i < product.size(); i++) {
-            quantity = quantity + product.get(i).quantity;
-            sum = sum + product.get(i).price * product.get(i).quantity;
-            name = name + product.get(i).name;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose what you need : 1 - all products;" +
+                "2 - beer; 3 - juice; 4 - water");
+
+        int enteredValue = scanner.nextInt();
+
+        switch (enteredValue) {
+            case 1:
+                for (int i = 0; i < product.size(); i++) {
+                    quantity = quantity + product.get(i).quantity;
+                    sum = sum + product.get(i).price * product.get(i).quantity;
+                    name = name + product.get(i).name;
+                }
+                break;
+            case 2:
+                for (int i = 0; i < product.size(); i++) {
+                    if (product.get(i) instanceof Beer) {
+                        quantity = quantity + product.get(i).quantity;
+                        sum = sum + product.get(i).price * product.get(i).quantity;
+                        name = name + product.get(i).name;
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < product.size(); i++) {
+                    if (product.get(i) instanceof Juice) {
+                        quantity = quantity + product.get(i).quantity;
+                        sum = sum + product.get(i).price * product.get(i).quantity;
+                        name = name + product.get(i).name;
+                    }
+                }
+                break;
+            case 4:
+                for (int i = 0; i < product.size(); i++) {
+                    if (product.get(i) instanceof Water) {
+                        quantity = quantity + product.get(i).quantity;
+                        sum = sum + product.get(i).price * product.get(i).quantity;
+                        name = name + product.get(i).name;
+                    }
+                }
+                break;
+            default:
+                for (int i = 0; i < product.size(); i++) {
+                    quantity = quantity + product.get(i).quantity;
+                    sum = sum + product.get(i).price * product.get(i).quantity;
+                    name = name + product.get(i).name;
+                }
         }
+
+
         System.out.println("We have products of such companies as " + name );
         System.out.println("The quantity of products is  " + quantity);
         System.out.println("The total price of products is " + sum + " hrn");
+
     }
+
 }
--- src/lesson3/Product.java
+++ src/lesson3/Product.java
@@ -3,8 +3,9 @@ package lesson3;
 /**
  * Created by panchenko_33 on 05.11.2016.
  */
-public abstract class Product {
+abstract class Product {
     String name;
     int price;
     int quantity;
 }
+
